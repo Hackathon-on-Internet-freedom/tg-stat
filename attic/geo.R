2b6b9f0aab801e7071cc80fa3eb08ba788708d27
Mansur Ziiatdinov <gltronred@gmail.com>
2020-06-06 15:49:51 +0300

    Более хороший формат данных для географии

--- attic/geo.R
+++ attic/geo.R
@@ -28,11 +28,8 @@ md <- df4 %>%
   left_join(df3, by=c("lev1","lev2","lev3"), suffix=c(".4",".3")) %>%
   left_join(df2, by=c("lev1","lev2"), suffix=c(".3",".2")) %>%
   left_join(df1, by=c("lev1"), suffix=c(".2",".1")) %>%
-  mutate(okato = okato.4,
-         okato.4 = NULL,
-         okato.3 = NULL,
-         okato.2 = NULL,
-         okato.1 = NULL) %>%
+  mutate(okato = okato.4) %>%
+  select(okato, name.4,lat.4,lon.4, name.3,lat.3,lon.3, name.2,lat.2,lon.2, name.1,lat.1,lon.1) %>%
   arrange(okato)
 
 write_csv(md, "/tmp/levels.csv")
