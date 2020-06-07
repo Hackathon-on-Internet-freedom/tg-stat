3747eed974c3fb9175b9b3f13575b71897fc7793
Mansur Ziiatdinov <gltronred@gmail.com>
2020-06-07 15:44:40 +0300

    Hash chain in Google Sheets

--- bot/src/CEC/Sheets.hs
+++ bot/src/CEC/Sheets.hs
@@ -5,6 +5,7 @@
 module CEC.Sheets where
 
 import CEC.Types
+import CEC.Keys
 
 import Control.Concurrent.STM
 import Control.Lens
@@ -16,33 +17,45 @@ import qualified Data.Map.Strict as M
 import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.Encoding as ET
 import Network.Google.Sheets
 import Network.Google
 import System.IO (stdout)
 
-sheetWorker :: Config -> TBQueue MsgItem -> IO ()
-sheetWorker cfg mq = forever $ do
-  let sheetId = tcSheets $ cfgTargets cfg
-  msg <- atomically $ readTBQueue mq
-  case msg of
-    MsgInfo ans -> appendRow cfg sheetId ans
-    MsgTrust (ts,src,tgt) -> appendGS sheetId "trust" SrcOpen
-      [ ValTime ts
-      , ValUser src
-      , ValUser tgt
-      ]
+type CC = (TVar Nonce, PublicKey, SecretKey, Key, TMVar Text)
 
-appendRow :: Config -> Text -> Map Text FieldVal -> IO Text
-appendRow cfg sheetId ans = do
+sheetWorker :: Config -> TBQueue MsgItem -> IO ()
+sheetWorker cfg mq = do
+  iv <- initNonce >>= newTVarIO
+  ph <- newTMVarIO ""
+  forever $ do
+    let sheetId = tcSheets $ cfgTargets cfg
+        theBot = cfgBot cfg
+        pk = bcPublicKey theBot
+        sk = bcSecretKey theBot
+        ek = bcEncryptKey theBot
+        cc = (iv,pk,sk,ek,ph)
+    msg <- atomically $ readTBQueue mq
+    case msg of
+      MsgInfo ans -> appendRow cfg sheetId cc ans
+      MsgTrust (ts,src,tgt) -> appendGS sheetId "trust" cc (cfgSourceType cfg)
+        [ ValTime ts
+        , ValUser src
+        , ValUser tgt
+        ]
+
+appendRow :: Config -> Text -> CC -> Map Text FieldVal -> IO Text
+appendRow cfg sheetId cc ans = do
   let fields = getFieldNames cfg
       orderedAns = map (ans M.!) fields
       srcType = cfgSourceType cfg
-  appendGS sheetId "raw" srcType orderedAns
+  appendGS sheetId "raw" cc srcType orderedAns
 
-appendGS :: Text -> Text -> SourceType -> [FieldVal] -> IO Text
-appendGS sheetId name srcType vals = do
-  let raws = concatMap (toJV srcType) vals
-      range = name <> "!A:" <> (T.pack $ pure $ chr $ ord 'A' + length raws - 1)
+appendGS :: Text -> Text -> CC -> SourceType -> [FieldVal] -> IO Text
+appendGS sheetId name cc srcType vals = do
+  raws <- encodeVals cc srcType vals
+  let range = name <> "!A:" <> (T.pack $ pure $ chr $ ord 'A' + length raws - 1)
       values = valueRange
         & vrValues .~ [raws]
         & vrRange .~ Just range
@@ -56,19 +69,30 @@ appendGS sheetId name srcType vals = do
   resp <- runResourceT . runGoogle env $ send req
   pure $ T.pack $ show resp
 
-toJV :: SourceType -> FieldVal -> [Value]
-toJV _ (ValInt n) = pure $ toJSON n
-toJV _ (ValFloat d) = pure $ toJSON d
-toJV _ (ValText t) = pure $ toJSON t
-toJV _ (ValEncrypt t) = ["nonce", toJSON $ "ENC(" <> t <> ")"]
-toJV _ (ValTime ts) = pure $ toJSON ts
-toJV _ (ValLoc loc) = map (toJSON . ($ loc))
+encodeVals :: CC -> SourceType -> [FieldVal] -> IO [Value]
+encodeVals cc@(_,pk,sk,_,ph) srcType vals = do
+  rs <- mapM (toJV cc srcType) vals
+  h <- atomically $ do
+    phv <- takeTMVar ph
+    let v = ET.decodeUtf8 $ encode $ concat $ rs ++ [[toJSON phv]]
+        hv = hash $ LT.toStrict v
+    putTMVar ph hv
+    pure hv
+  pure $ concat rs ++ [toJSON h, toJSON ("signature" :: Text)]
+
+toJV :: CC -> SourceType -> FieldVal -> IO [Value]
+toJV _ _ (ValInt n) = pure $ pure $ toJSON n
+toJV _ _ (ValFloat d) = pure $ pure $ toJSON d
+toJV _ _ (ValText t) = pure $ pure $ toJSON t
+toJV (iv,pk,sk,ek,ph) _ (ValEncrypt t) = pure $ ["nonce", toJSON $ "ENC(" <> t <> ")"]
+toJV _ _ (ValTime ts) = pure $ pure $ toJSON ts
+toJV _ _ (ValLoc loc) = pure $ map (toJSON . ($ loc))
   [ locCity
   , fromMaybe "" . locMunicip
   , fromMaybe "" . locRegion
   , locSubject
   ]
-toJV srcType (ValUser u) = case srcType of
+toJV (iv,pk,sk,ek,ph) srcType (ValUser u) = pure $ case srcType of
   SrcOpen -> pure $ toJSON u
   SrcHashed -> pure $ toJSON $ "HASH" <> "(" <> u <> ")"
   SrcEncrypted -> [ toJSON ("nonce" :: Text), toJSON $ "ENCRYPT(" <> u <> ")" ]
